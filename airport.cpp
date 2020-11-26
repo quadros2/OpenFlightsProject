b088c963b690a7a77c86616148f72ea594018c0b
Sabar Nimmagadda <sabarn2@linux-a2.ews.illinois.edu>
2020-11-25 19:59:46 -0600

    Added structural files for airports and directed graphs.

---
+++ airport.cpp
@@ -0,0 +1,10 @@
+#include <airport.h>
+#include <iostream>
+using std::cout; 
+using std::string;
+
+
+airport::airport(int ide) {
+    id = ide;
+}
+
---
+++ directed_edge.h
@@ -0,0 +1,13 @@
+#ifndef DIRECTED_EDGE_H
+#define DIRECTED_EDGE_H
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+ class directed_edge {
+     
+ };
+
+
+
+#endif //DIRECTED_EDGE_H
