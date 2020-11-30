da9bcbed03c03f1fe41d065248d91bc21c0c7919
Hrishikesh Deshmukh <hcd3@linux-a2.ews.illinois.edu>
2020-11-30 17:30:57 -0600

    Create files for testing

---
+++ tests/tests.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+#include "../tests/catch.hpp"
+#include "../graph.h"
+#include "../Airport.h"
+
+
+TEST_CASE("Testing file to string") {
+    REQUIRE ("Sabar" == "Hrishi");
+}
